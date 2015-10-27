5076451959adfb58919a524e24b750f6dc2d55b2
Fanch <dev@xxii.io>
2015-10-27 14:52:42 +0100

    Add framework to workspace

---
+++ VRKit/VRKit/VRKit.h
@@ -0,0 +1,19 @@
+//
+//  VRKit.h
+//  VRKit
+//
+//  Created by François on 27/10/2015.
+//  Copyright © 2015 XXII IO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VRKit.
+FOUNDATION_EXPORT double VRKitVersionNumber;
+
+//! Project version string for VRKit.
+FOUNDATION_EXPORT const unsigned char VRKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VRKit/PublicHeader.h>
+
+
